64d3cf94b24ac46e69342f3f18cfbab8a6c64d83
Vishal Dhanjani <vdhanjani63@gmail.com>
2020-08-18 15:40:30 +0530

    Add files via upload

---
+++ Warm-up Challenges/Jumping on the Clouds.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the jumpingOnClouds function below.
+int jumpingOnClouds(vector<int> c) {
+    int jump;
+    jump=0;
+    int index;
+    index=0;
+    if(c[0]==0&&c[1]==0&&c.size()==2){
+        return 1;
+    }
+    while(index!=c.size()-1){
+        if(c[index+2]==0){
+            jump++;
+            index=index+2;
+            continue;
+        }
+        else if(c[index+1]==0){
+            jump++;
+            index=index+1;
+            continue;
+        }
+        else if(c[index+1]==1){
+            index=index+2;
+            continue;
+        }
+    }
+
+    return jump;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int result = jumpingOnClouds(c);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
