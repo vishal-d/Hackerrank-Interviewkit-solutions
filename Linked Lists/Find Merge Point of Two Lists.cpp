9bd4c28e586dab569daff5b338489e490e882d98
Vishal Dhanjani <vdhanjani63@gmail.com>
2020-09-30 18:53:58 +0530

    Update Find Merge Point of Two Lists.cpp

--- Linked Lists/Find Merge Point of Two Lists.cpp
+++ Linked Lists/Find Merge Point of Two Lists.cpp
@@ -11,6 +11,8 @@ int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
     SinglyLinkedListNode* check1=head1;
     SinglyLinkedListNode* check2=head2;
     SinglyLinkedListNode* temp=head2;
+    unordered_map<SinglyLinkedListNode*,int>umap;
+
     int length1=0, length2=0;
     while(check1!=NULL){
         length1++;
@@ -20,13 +22,22 @@ int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
         length2++;
         check2=check2->next;
     }
+
+    for(int i=0; i<length2; i++){
+        umap[head2]++;
+        head2=head2->next;
+    }
+    head2=temp;
     for(int i=0; i<length1; i++){
         head2=temp;
-        for(int j=0; j<length2; j++){
+        /*for(int j=0; j<length2; j++){
             if(head1==head2){
                 return head1->data;
             }
             head2=head2->next;
+        }*/
+        if(umap.find(head1)!=umap.end()){
+            return head1->data;
         }
         head1=head1->next;
     }
